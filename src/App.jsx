7f3bc62afda3fffda6aa00cfa93b2580b0066131
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-11-01 20:07:39 -0300

    Ventana parrallax terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import Lautaro from "./Components/lautaro/Lautaro";
 import Navbar from "./Components/navbar/Navbar";
+import Parrallax from "./Components/parrallax/Parrallax";
 // import Test from "./Components/test/Test";
 
 function App() {
@@ -10,9 +11,17 @@ function App() {
         <Navbar />
         <Lautaro />
       </section>
-      <section id="Services">Services</section>
-      <section id="Portfolio">Portfolio</section>
-      <section id="Contact">Contact</section>
+      <section id="Services">
+        <Parrallax type="services" />
+      </section>
+      <section>Servicios</section>
+      <section id="Portfolio">
+        <Parrallax tpe="portfolio" />
+      </section>
+      <section>Portafolio1</section>
+      <section>Portafolio2</section>
+      <section>Portafolio3</section>
+      <section id="Contact">Contacto</section>
       {/* <Test /> */}
     </>
   );
